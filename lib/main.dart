3dccd32384e4ed7ea47dc41cfaca39641b48da0f
Zhur.a.vleva <Zhur.a.vleva.Darya21@gmail.com>
2022-06-18 14:18:08 +0300

    [ADDED]: search page

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,12 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-const String homePage = "Home";
-const String favouritePage = "Favorite";
-
 class _MyHomePageState extends State<MyHomePage> {
   late Future<Joke> _joke;
   late List<Widget> _pages;
   late Widget _home;
   late Widget _favorite;
+  late Widget _search;
   late int _selectedIndex;
   late Widget _currentPage;
 
@@ -82,7 +80,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _home = const HomeStateful();
     _favorite = const FavoriteStateful();
-    _pages = [_home, _favorite];
+    _search = const SearchStateful();
+    _pages = [_home, _favorite, _search];
     _currentPage = _home;
     _selectedIndex = 0;
     WidgetsBinding.instance.addPostFrameCallback(
@@ -92,9 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Jokes about Chuck Norris"),
-      ),
+      appBar: AppBar(title: const Text("Jokes about Chuck Norris")),
       body: _currentPage,
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -106,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.favorite),
             label: 'Favorite',
           ),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search')
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
@@ -315,5 +313,78 @@ class Favorite extends State<FavoriteStateful> {
                             icon: Icon(Icons.delete))
                       ])));
         });
+  }
+}
+
+class SearchStateful extends StatefulWidget {
+  const SearchStateful({Key? key}) : super(key: key);
+
+  @override
+  State<SearchStateful> createState() => Search();
+}
+
+class Search extends State<SearchStateful> {
+  TextEditingController editingController = TextEditingController();
+  List<Joke> items = [];
+
+  Future<List<Joke>> fetchJokes(String query) async {
+    final response = await http
+        .get(Uri.parse('https://api.chucknorris.io/jokes/search?query=$query'));
+    if (response.statusCode == 200) {
+      var body = jsonDecode(response.body);
+      var result = body["result"] as List;
+      return result.map<Joke>((json) => Joke.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load Jokes');
+    }
+  }
+
+  void filterSearchResults(String query) {
+    Future<List<Joke>> list = fetchJokes(query);
+    list.then((value) => setState(() {
+          items = value;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding:
+              const EdgeInsets.only(left: 8, right: 8, bottom: 10, top: 15),
+          child: TextField(
+            onChanged: (value) {
+              filterSearchResults(value);
+            },
+            controller: editingController,
+            decoration: const InputDecoration(
+                labelText: "Search",
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+          ),
+        ),
+        Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      color: Colors.white,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Flexible(
+                            child: Text(items[index].value,
+                                textAlign: TextAlign.justify)),
+                      ));
+                })),
+      ],
+    );
   }
 }
